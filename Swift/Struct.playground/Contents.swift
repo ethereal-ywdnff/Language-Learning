0e888758e7f7fff350c8fdd2cb34516b08fcf3c6
Kanghua Liu <104200902+ethereal-ywdnff@users.noreply.github.com>
2024-06-09 00:32:10 +0100

    get-set, willSet-didSet, subscript

--- Swift/Struct.playground/Contents.swift
+++ Swift/Struct.playground/Contents.swift
@@ -52,3 +52,71 @@ print("a1:", a1.age)
 var a2 = a1 // 值传递，而不是传引用
 a2.age = 100
 print("a1: \(a1.age), a2: \(a2.age)")
+
+
+struct Person {
+    private var value = ""
+    var name:String {
+        set(name) {
+            value = value + name + " "
+            print("set",name)
+        }
+        get {
+//            print("get ")
+            return "name: " + value
+        }
+    }
+    
+    var age:Int = 18 {
+//        willSet(new) {
+//            print("latest value:",new)
+//        }
+//        didSet(old) {
+//            print("previous value:",old)
+//        }
+        willSet {
+            print("latest value:",newValue)
+        }
+        didSet {
+            print("previous value:",oldValue)
+        }
+    }
+    
+    var chapters: [String] = ["第一章: 开始", "第二章: 进行", "第三章: 结束"]
+
+    // 使用subscript定义下标脚本
+    subscript(index: Int) -> String {
+        get {
+            // 确保索引在合理范围内
+            if index >= 0 && index < chapters.count {
+                return chapters[index]
+            } else {
+                return "索引超出范围"
+            }
+        }
+        set {
+            // 仅当索引在范围内时替换值
+            if index >= 0 && index < chapters.count {
+                chapters[index] = chapters[index] + " " + newValue
+            } else {
+                print("no such index(\(index)), so \(newValue) is been inserted to the chapters")
+                chapters.append(newValue)
+            }
+        }
+    }
+}
+
+var person = Person()
+person.name = "tim"
+print(person.name)
+person.name = "tom"
+print(person.name)
+print("-------------------")
+print("age",person.age)
+person.age = 20
+print("age",person.age)
+print("-------------------")
+person[0] = "新的开始"
+print(person.chapters)
+person[5] = "🤡"
+print(person.chapters)
